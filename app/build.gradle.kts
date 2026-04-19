e6c4e69a353d21adb782b5fe0327f9ad0ce10757
Javi Granados <jjavigrana2@gmail.com>
2026-04-19 15:21:29 +0200

    Plugin serialización

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    //Necessari per a la serialització
+    id ("kotlinx-serialization")
 }
 
 android {
